0df607b9320dbc74763044f4dd6c6929e88c8c74
Leigh Anderson <landerson@equalexperts.com>
2018-04-18 07:49:06 +0100

    Use SSL from ingress to backend.

--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -jar /app/noninteractive-doc-check.jar
+java ${JAVA_OPTS} -Dspring.profiles.active=ssl -Djava.security.egd=file:/dev/./urandom -jar /app/noninteractive-doc-check.jar
